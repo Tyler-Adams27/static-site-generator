ce7547251b65c352764e5e4cdee59e60b6543440
tyler-adams27 <tyler.adams.prof@gmail.com>
2025-03-10 21:16:25 +0000

    Started

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import *
+
+test_text = "Hello World"
+test_link = "link"
+test_url = "https://www.google.com"
+
---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TextType(Enum):
+    text_bold = "bold"
+    text_italic = "italic"
+    text_code = "code"
+    text_link = "link"
+    text_url = "url"
+
+
+
