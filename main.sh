e1d61dc42cfd961e345b7d32f7d6762ed2e7da87
Tyler-Adams27 <tyler.adams.prof@gmail.com>
2025-03-12 11:07:38 +0000

    Finished CH2 L1

--- main.sh
+++ main.sh
@@ -1,3 +1,2 @@
 python3 src/main.py
-
 ./main.sh
--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,34 @@
 from enum import Enum
 
+test_text = "Hello World"
+test_link = "link"
+test_url = "https://www.google.com"
+
 class TextType(Enum):
+    normal_text = "normal"
     text_bold = "bold"
     text_italic = "italic"
     text_code = "code"
     text_link = "link"
-    text_url = "url"
+    text_image = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self):
+        text_type_value = self.text_type.value if hasattr(self.text_type, 'value') else self.text_type
+        return f"TextNode({self.text}, {text_type_value}, {self.url})"
 
 
+def main():
+    print(TextNode(test_text,test_link,test_url))
+        
+
+main()
 
