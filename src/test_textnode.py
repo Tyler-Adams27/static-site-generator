a38fe57a015f749c3580c7192fae4cb7e2ba8376
Tyler.Adams27 <tyler.adams.prof@gmail.com>
2025-03-14 14:25:41 +0000

    Started

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,6 +7,9 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
 
+        self.assertEqual(node == node2)
+        self.assertNotEqual(node != node2)
+
 
 if __name__ == "__main__":
     unittest.main()
