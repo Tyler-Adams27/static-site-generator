7a722fa7a21cc1d5e3f1b1d0902e066164365bde
Tyler.Adams27 <tyler.adams.prof@gmail.com>
2025-03-17 10:27:29 +0000

    Added LeafNode and LeafNode Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-
+import unittest
 
 class HTMLNode():
     def __init__(self, tag = None, value = None, children = None, props = None):
@@ -20,3 +20,26 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag = None, value = None, props = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+            
+
+
+
+                
+
+                
+
+
+
---
+++ src/test_leafnode.py
@@ -0,0 +1,22 @@
+import unittest
+from htmlnode import *
+
+class test_leadnode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
