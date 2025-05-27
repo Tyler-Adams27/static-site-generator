df54b1e59cb0a953a401bb5abfa2b84ac8d553d1
Tyler Adams <tyler.adams.prof@gmail.com>
2025-05-27 15:47:00 +0100

    idk

---
+++ src/textnode.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class TextType():
+    BOLD_TEXT = "**Bold Text**"
+    ITALIC_TEXT = "_Italic Text_"
+    CODE_TEXT = "`code text`"
+    LINKS = "links (google.com)"
+    IMAGES = "image"
